b779a2c8b3136224bbc4b8bd1f7bc71a942f8985
plamenna-petrova <59893040+plamenna-petrova@users.noreply.github.com>
2023-02-20 00:19:57 +0200

    Added the structural code for the Factory pattern

---
+++ InformaticsDesignPatternsGoF/Creational/Factory/Structural Code/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Structural_Code
+{
+    public abstract class Product
+    {
+
+    }
+
+    public class ConcreteProductA: Product
+    {
+
+    }
+
+    public class ConcreteProductB: Product
+    {
+
+    }
+
+    public abstract class Creator
+    {
+        public abstract Product CreateProduct();
+    }
+
+    public class ConcreteCreatorA : Creator
+    {
+        public override Product CreateProduct()
+        {
+            return new ConcreteProductA();
+        }
+    }
+
+    public class ConcreteCreatorB : Creator
+    {
+        public override Product CreateProduct()
+        {
+            return new ConcreteProductB();
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Creator[] creators = new Creator[2];
+
+            creators[0] = new ConcreteCreatorA();
+            creators[1] = new ConcreteCreatorB();
+
+            foreach (Creator creator in creators)
+            {
+                Product product = creator.CreateProduct();
+                Console.WriteLine($"Created: {product.GetType().Name}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
