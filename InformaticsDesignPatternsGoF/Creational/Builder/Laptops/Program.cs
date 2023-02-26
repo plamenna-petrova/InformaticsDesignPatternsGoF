b10be63cf4d0026ff88e851ea86de8c8224aef64
plamenna-petrova <59893040+plamenna-petrova@users.noreply.github.com>
2023-02-26 18:49:55 +0200

    Added Laptops project for the Builder pattern

---
+++ InformaticsDesignPatternsGoF/Creational/Builder/Laptops/Program.cs
@@ -0,0 +1,26 @@
+﻿using Laptops.Builder;
+using Laptops.ConcreteBuilders;
+using Laptops.Director;
+using System;
+using System.Text;
+
+namespace Laptops
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LaptopBuilder laptopBuilder = null;
+
+            LaptopStore laptopStore = new LaptopStore();
+
+            laptopBuilder = new ASUSConcreteBuilder();
+            laptopStore.BuildLaptopConfiguration(laptopBuilder);
+            laptopBuilder.Laptop.ShowDetails();
+
+            laptopBuilder = new LenovoConcreteBuilder();
+            laptopStore.BuildLaptopConfiguration(laptopBuilder);
+            laptopBuilder.Laptop.ShowDetails();
+        }
+    }
+}
