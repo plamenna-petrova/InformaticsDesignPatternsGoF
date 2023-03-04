246a8bdc3afeb811649468c473dea186ab6b035f
plamenna-petrova <59893040+plamenna-petrova@users.noreply.github.com>
2023-03-04 16:52:29 +0200

    Added base Employees project for the Prototype pattern

---
+++ InformaticsDesignPatternsGoF/Creational/Prototype/Employees/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Employees
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+
+        public string Department { get; set; }
+
+        public Employee GetClone()
+        {
+            return (Employee)this.MemberwiseClone();
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            employee1.Name = "John";
+            employee1.Department = "IT";
+
+            Employee employee2 = employee1.GetClone();
+            employee2.Name = "James";
+
+            Console.WriteLine($"Employee #1: {employee1.Name} - {employee1.Department}");
+            Console.WriteLine($"Employee #2: {employee2.Name} - {employee2.Department}");
+        }
+    }
+}
