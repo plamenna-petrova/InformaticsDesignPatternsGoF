49cba33682d8454e475199387d3cc35e4fd42b50
plamenna-petrova <59893040+plamenna-petrova@users.noreply.github.com>
2023-03-04 17:09:00 +0200

    Added Shallow and Deep Copies distinction for the Employees project

--- InformaticsDesignPatternsGoF/Creational/Prototype/Employees/Program.cs
+++ InformaticsDesignPatternsGoF/Creational/Prototype/Employees/Program.cs
@@ -2,15 +2,47 @@
 
 namespace Employees
 {
-    public class Employee
+    public abstract class Employee
     {
         public string Name { get; set; }
 
         public string Department { get; set; }
 
-        public Employee GetClone()
+        public Address Address { get; set; }
+
+        public abstract Employee GetShallowCopy();
+
+        public abstract Employee GetDeepCopy();
+
+        public void GetDetails()
         {
-            return (Employee)this.MemberwiseClone();
+            Console.WriteLine($"Employee Details: Name - {this.Name}, " +
+               $"Department - {this.Department}, Address - {this.Address.Name}");
+        }
+    }
+
+    public class SoftwareDeveloper : Employee
+    {
+        public override Employee GetShallowCopy()
+        {
+            return (Employee) this.MemberwiseClone();
+        }
+
+        public override Employee GetDeepCopy()
+        {
+            Employee clonedEmployee = this.GetShallowCopy();
+            clonedEmployee.Address = Address.GetClone();
+            return clonedEmployee;
+        }
+    }
+
+    public class Address
+    {
+        public string Name { get; set; } 
+
+        public Address GetClone()
+        {
+            return (Address) this.MemberwiseClone();
         }
     }
 
@@ -18,15 +50,23 @@ namespace Employees
     {
         static void Main(string[] args)
         {
-            Employee employee1 = new Employee();
-            employee1.Name = "John";
-            employee1.Department = "IT";
+            SoftwareDeveloper softwareDeveloper1 = new SoftwareDeveloper();
+            softwareDeveloper1.Name = "John";
+            softwareDeveloper1.Department = "IT";
+            softwareDeveloper1.Address = new Address { Name = "London, UK" };
 
-            Employee employee2 = employee1.GetClone();
-            employee2.Name = "James";
+            SoftwareDeveloper softwareDeveloper2 = (SoftwareDeveloper) softwareDeveloper1.GetShallowCopy();
 
-            Console.WriteLine($"Employee #1: {employee1.Name} - {employee1.Department}");
-            Console.WriteLine($"Employee #2: {employee2.Name} - {employee2.Department}");
+            SoftwareDeveloper softwareDeveloper3 = (SoftwareDeveloper) softwareDeveloper1.GetDeepCopy();
+
+            softwareDeveloper2.Name = "James";
+            softwareDeveloper2.Address.Name = "New York, USA";
+
+            softwareDeveloper3.Address.Name = "Barcelona, Spain";
+
+            softwareDeveloper1.GetDetails();
+            softwareDeveloper2.GetDetails();
+            softwareDeveloper3.GetDetails();
         }
     }
 }
