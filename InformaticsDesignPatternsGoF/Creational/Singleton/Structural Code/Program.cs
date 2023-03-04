ce055be9a9ee0b9628dfe5a2bbc6f514765965b2
plamenna-petrova <59893040+plamenna-petrova@users.noreply.github.com>
2023-03-04 17:19:02 +0200

    Added the structural code for the Singleton pattern

---
+++ InformaticsDesignPatternsGoF/Creational/Singleton/Structural Code/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Structural_Code
+{
+    public class Singleton
+    {
+        static Singleton instance;
+
+        protected Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+
+            return instance;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singleton1 = Singleton.GetInstance();
+            Singleton singleton2 = Singleton.GetInstance();
+
+            if (singleton1 == singleton2)
+            {
+                Console.WriteLine("The objects share the same instance");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
