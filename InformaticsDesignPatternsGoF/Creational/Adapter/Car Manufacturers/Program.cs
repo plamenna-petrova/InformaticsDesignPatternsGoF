d7a137f1fd292a7f74e085df506b350d4b202e3f
plamenna-petrova <59893040+plamenna-petrova@users.noreply.github.com>
2023-03-11 16:15:08 +0200

    Finished the Car Manufacturers project for the Adapter pattern

--- InformaticsDesignPatternsGoF/Creational/Adapter/Car Manufacturers/Program.cs
+++ InformaticsDesignPatternsGoF/Creational/Adapter/Car Manufacturers/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
@@ -48,12 +49,61 @@ namespace Car_Manufacturers
         }
     }
 
+    public class JsonConverter
+    {
+        private IEnumerable<Manufacturer> manufacturers;
+
+        public JsonConverter(IEnumerable<Manufacturer> manufacturers)
+        {
+            this.manufacturers = manufacturers;
+        }
+
+        public void ConvertToJson()
+        {
+            var jsonManufacturers = JsonConvert.SerializeObject(manufacturers, Formatting.Indented);
+
+            Console.WriteLine("\nPrinting JSON list\n");
+            Console.WriteLine(jsonManufacturers);
+        }
+    }
+
+    public interface IXMLToJson
+    {
+        void ConvertXMLToJson();
+    }
+
+    public class XMLToJsonAdapter : IXMLToJson
+    {
+        private readonly XmlConverter xmlConverter;
+
+        public XMLToJsonAdapter(XmlConverter xmlConverter)
+        {
+            this.xmlConverter = xmlConverter;
+        }
+
+        public void ConvertXMLToJson()
+        {
+            var manufacturers = xmlConverter.GetXML()
+                .Elements("Manufacturers")
+                .Elements("Manufacturer")
+                .Select(m => new Manufacturer
+                {
+                    City = m.Attribute("City").Value,
+                    Name = m.Attribute("Name").Value,
+                    Year = Convert.ToInt32(m.Attribute("Year").Value)
+                });
+
+            new JsonConverter(manufacturers).ConvertToJson();
+        }
+    }
+
     public class Program
     {
         static void Main(string[] args)
         {
             XmlConverter xmlConverter = new XmlConverter();
-            xmlConverter.GetXML();
+            IXMLToJson xmlToJsonAdapter = new XMLToJsonAdapter(xmlConverter);
+            xmlToJsonAdapter.ConvertXMLToJson();
         }
     }
 }
