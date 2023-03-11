9dc5b7c5fe58cbdcc5649aa0e009bb98ff8f1e62
plamenna-petrova <59893040+plamenna-petrova@users.noreply.github.com>
2023-03-11 17:59:32 +0200

    Added the structural code for the bridge pattern

---
+++ InformaticsDesignPatternsGoF/Structural/Bridge/Sturctural Code/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Sturctural_Code
+{
+    public class Abstraction
+    {
+        protected Implementor implementor;
+
+        public Implementor Implementor
+        {
+            set { implementor = value; }
+        }
+
+        public virtual void ExecuteOperation()
+        {
+            implementor.ExecuteOperation();
+        }
+    }
+
+    public abstract class Implementor
+    {
+        public abstract void ExecuteOperation();
+    }
+
+    public class RefinedAbstraction : Abstraction
+    {
+        public override void ExecuteOperation()
+        {
+            implementor.ExecuteOperation();
+        }
+    }
+
+    public class ConcreteImplementorA : Implementor
+    {
+        public override void ExecuteOperation()
+        {
+            Console.WriteLine("ConcreteImplementorA Operation");
+        }
+    }
+
+    public class ConcreteImplementorB : Implementor
+    {
+        public override void ExecuteOperation()
+        {
+            Console.WriteLine("ConcreteImplementorB Operation");
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Abstraction abstraction = new RefinedAbstraction();
+
+            abstraction.Implementor = new ConcreteImplementorA();
+            abstraction.ExecuteOperation();
+
+            abstraction.Implementor = new ConcreteImplementorB();
+            abstraction.ExecuteOperation();
+
+            Console.ReadKey();
+        }
+    }
+}
