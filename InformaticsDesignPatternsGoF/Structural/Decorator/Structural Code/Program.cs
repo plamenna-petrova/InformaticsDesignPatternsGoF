7a4e18a4da5bca3d63d416482948db97636ec054
plamenna-petrova <59893040+plamenna-petrova@users.noreply.github.com>
2023-03-17 22:35:32 +0200

    Added Structural Code for the Decorator pattern

---
+++ InformaticsDesignPatternsGoF/Structural/Decorator/Structural Code/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Structural_Code
+{
+    public abstract class Component
+    {
+        public abstract void ExecuteOperation();
+    }
+
+    public class ConcreteComponent : Component
+    {
+        public override void ExecuteOperation()
+        {
+            Console.WriteLine("Called ExecuteOperation() from Concrete Component");
+        }
+    }
+
+    public abstract class Decorator : Component
+    {
+        protected Component component;
+
+        public void SetComponent(Component component)
+        {
+            this.component = component;
+        }
+
+        public override void ExecuteOperation()
+        {
+            if (component != null)
+            {
+                component.ExecuteOperation();
+            }
+        }
+    }
+
+    public class ConcreteDecoratorA : Decorator
+    {
+        public override void ExecuteOperation()
+        {
+            base.ExecuteOperation();
+            Console.WriteLine("Called ExecuteOperation() from ConcreteDecoratorA");
+        }
+    }
+
+    public class ConcreteDecoratorB : Decorator
+    {
+        public override void ExecuteOperation()
+        {
+            base.ExecuteOperation();
+            AddBehavior();
+            Console.WriteLine("Called ExecuteOperation() from ConcreteDecoratorB");
+        }
+
+        public void AddBehavior()
+        {
+
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcreteComponent concreteComponent = new ConcreteComponent();
+            ConcreteDecoratorA concreteDecoratorA = new ConcreteDecoratorA();
+            ConcreteDecoratorB concreteDecoratorB = new ConcreteDecoratorB();
+
+            concreteDecoratorA.SetComponent(concreteComponent);
+            concreteDecoratorB.SetComponent(concreteDecoratorA);
+
+            concreteDecoratorB.ExecuteOperation();
+
+            Console.ReadKey();
+        }
+    }
+}
