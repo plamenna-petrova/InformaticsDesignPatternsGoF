21ea84b2ea4843cd96ab7eb5cad590a4a79b1094
plamenna.petrova <plamennavp@abv.bg>
2023-12-22 00:35:37 +0200

    Added examples for the interpreter pattern

---
+++ InformaticsDesignPatternsGoF/Behavioral/Interpreter/Structural Code/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Structural_Code
+{
+    public class Context
+    {
+
+    }
+
+    public abstract class AbstractExpression
+    {
+        public abstract void Interpret(Context context);
+    }
+
+    public class TerminalExpression : AbstractExpression
+    {
+        public override void Interpret(Context context)
+        {
+            Console.WriteLine("Called Terminal.Interpret()");
+        }
+    }
+
+    public class NonterminalExpression : AbstractExpression
+    {
+        public override void Interpret(Context context)
+        {
+            Console.WriteLine("Called Nonterminal.Interpret()");
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context = new Context();
+
+            /* 
+                Usually a tree
+                Populate 'abstract syntax tree'
+            */
+
+            List<AbstractExpression> listOfExpressions = new List<AbstractExpression>
+            {
+                new TerminalExpression(),
+                new NonterminalExpression(),
+                new TerminalExpression(),
+                new TerminalExpression()
+            };
+
+            foreach (AbstractExpression expression in listOfExpressions)
+            {
+                expression.Interpret(context);
+            }
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+}
